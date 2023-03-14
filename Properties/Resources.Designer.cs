913f6c5f30b5d39f7341fb7cf9865909f2b8f754
Владимир Конев <v.konev@smcapital.ru>
2023-03-14 19:06:16 +0300

    Change Namespace

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoodProgramManagmentTools.Properties {
+namespace BpmSoftManagmentTools.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GoodProgramManagmentTools.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoodProgramManagmentTools.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BpmSoftManagmentTools.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoodProgramManagmentTools.Properties {
+namespace BpmSoftManagmentTools.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
